06242e2f5ba9df9beb4476fa10d62004ddfbf63a
MiltonMora <mayloevan@gmail.com>
2020-12-27 18:50:32 +0100

    custom

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import AppContext from '../context/AppContext';
 
 import '../styles/components/Success.css';
 
 const Success = () => {
+
+  const {state: {buyer}} = useContext(AppContext);
+
   return (
     <div className="Succes">
       <div className="Success-content">
-        <h2>Nombre, Gracias por tu compra</h2>
+        <h2>{`${buyer.name}, Gracias por tu compra`}</h2>
         <span>Tu pedido llegara en 3 dias a tu dirección:</span>
-        <div className="Success-map">Google Maps</div>
+        <div className="Success-map">
+          
+        </div>
       </div>
     </div>
   );
